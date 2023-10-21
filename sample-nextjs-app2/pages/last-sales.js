5cf8a4743a6ba41bfa408f5d1fa85972290cd37b
Dhineshbabu Muthuraj <dhineshbabumuthuraj@Dhineshbabus-MacBook-Pro.local>
2023-10-20 22:20:06 -0700

    added concept code for pre-rendering

---
+++ sample-nextjs-app2/pages/last-sales.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+
+const LastSalesPage = () => {
+  const [sales, setSales] = useState();
+//   const [isLoading, setIsLoading] = useState(false);
+const fetcher = (url) => fetch(url).then((res) => res.json());
+ const {data, error} =  useSWR('https://react-kani-app-default-rtdb.firebaseio.com/sales.json', fetcher);
+
+ useEffect(() => {
+    
+    if(data) {
+        const transformedSales = [];
+        for (const key in data) {
+            transformedSales.push({
+              id: key,
+              username: data[key].username,
+              volume: data[key].volume,
+            });
+          }
+
+          setSales(transformedSales);
+    }
+ }, [data]);
+
+ /*
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://react-kani-app-default-rtdb.firebaseio.com/sales.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const transformedSales = [];
+        for (const key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+        setSales(transformedSales);
+        setIsLoading(false);
+      });
+  }, []);
+
+  */
+
+  if(error) {
+    return <p>Failed to load ....</p>
+  }
+
+  if(!data || !sales ) {
+    return <p>Loading ....</p>
+  }
+
+
+
+  return <ul>
+    {sales.map(sale => <li key={sale.id}>
+        {sale.username} - ${sale.volume}
+    </li>)}
+  </ul>;
+};
+
+export default LastSalesPage;
